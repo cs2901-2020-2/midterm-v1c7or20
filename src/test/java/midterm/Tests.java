efea70abf4dc7f539215730a1b51e76aaca23739
v1c7or20 <victor.pena@utec.edu.pe>
2020-11-05 10:08:26 -0500

    test

--- src/test/java/midterm/Tests.java
+++ src/test/java/midterm/Tests.java
@@ -41,4 +41,22 @@ public class Tests {
         File file = new File("dummyPath3.jpeg");
         Assert.assertTrue(file.exists());
     }
+
+    @Test
+    public void testSubject() throws IOException{
+        Subject subject = new Subject();
+        DataPoint<String, Integer> dat1 = subject.addData("dat1", 1);
+        DataPoint<String, Integer> dat2 = subject.addData("dat2", 2);
+        DataPoint<String, Integer> dat3 = subject.addData("dat3", 3);
+        Assert.assertEquals("dat1", dat1.getIndex());
+        Assert.assertEquals((Integer) 1, dat1.getValue());
+        Assert.assertEquals("dat2", dat2.getIndex());
+        Assert.assertEquals((Integer) 2, dat2.getValue());
+        Assert.assertEquals("dat3", dat3.getIndex());
+        Assert.assertEquals((Integer) 3, dat3.getValue());
+        DataPoint<String, Integer> dat1R = subject.removeData("dat1");
+        DataPoint<String, Integer> datNR = subject.removeData("datN");
+        Assert.assertNull(datNR);
+        Assert.assertEquals(dat1R.getIndex(), dat1.getIndex());
+    }
 }
